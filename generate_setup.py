d9a0510ff656c3933377a7187a70c7987d13283e
NikBarykin <barykin.nikita63@gmail.com>
2023-07-09 00:16:15 +0700

    minor changes

--- generate_setup.py
+++ generate_setup.py
@@ -1,61 +1,65 @@
+from user import User
+from constants import (
+        SEXES,
+        CITIES,
+        RELATIONSHIP_GOALS,
+        )
+
+from generate.config import (
+        MIN_AGE,
+        MAX_AGE,
+        MALE_NAMES,
+        FEMALE_NAMES,
+        MALE_PHOTOS,
+        FEMALE_PHOTOS,
+        MALE_SELF_DESCRIPTIONS,
+        FEMALE_SELF_DESCRIPTIONS,
+        )
+
 import random
-from agent import Agent, AVAILABLE_CITIES
 
-# MALE_NAMES = ("Ваня", "Олег", "Леша", "Макс", "Егор")
-MALE_NAMES = tuple()
-MALE_PICTURES = [
-        "AgACAgIAAxkBAAPTZJ6UzwzkGrBNKoCREV_wOZZeS-sAAsHMMRt--PFIAAHRjBjaArQ9AQADAgADcwADLwQ",
-        "AgACAgIAAxkBAAPaZJ6Vi7G_R-71mT-BgVCRB5BIMdkAAsjMMRt--PFIvhO86_6XvlMBAAMCAANzAAMvBA"]
+from sqlalchemy import engine
 
 
-FEMALE_NAMES = (
-        "Лия",
-        "Анастасия",
-        "Александра",
-        "Дарья",
-        "Анна",
-        "Мария",
-        "Елена",
-        "Ксения",
-        "София",
-        "Екатерина",
-        "Наталья",
-        "Вероника",
-        "Валерия",
-        "Диана",
-        "Маргарита",
-        "Юлия",
-        "Алина",
-        "Ольга",
-        "Арина",
-        "Татьяна",
-        )
+for i in range(NO_USERS):
+    telegram_id = i
+    telegram_handle = "imaginary_handle"
+    sex = random.choice(SEXES)
+    age = random.randint(MIN_AGE, MAX_AGE)
 
-FEMALE_PICTURES = (
-        "AgACAgIAAxkBAAPRZJ6UlQOepvv5gbtP52npF5jk7p4AAsLMMRt--PFIDlOStB3xcjsBAAMCAANzAAMvBA",
-        "AgACAgIAAxkBAAPXZJ6VakWrRZRSYDJxwqBXNnR-NxMAAsfMMRt--PFI_F7X1LUUmAEBAAMCAANzAAMvBA",
-        "AgACAgIAAxkBAAIC4GSf6Ng8d4O0VhwX8o-UQlbXRidmAAIEyjEbfvgBSc5S0y8hYOmiAQADAgADcwADLwQ",
-        "AgACAgIAAxkBAAIC4mSf6O5yL0g_QMF9t2UhVR44xbmGAAIFyjEbfvgBSXY_4yV1vkmEAQADAgADcwADLwQ"
-        )
+    if sex == "мужчина":
+        names = MALE_NAMES
+        photos = MALE_PHOTOS
+        descriptions = MALE_SELF_DESCRIPTIONS
+    else:
+        names = FEMALE_NAMES
+        photos = FEMALE_PHOTOS
+        descriptions = FEMALE_SELF_DESCRIPTIONS
 
-MALE_SELF_DESCRIPTIONS = (
-        "Я активный, веселый, дружелюбный парень со стремлением к достижению больших вещей. Ищу партнершу, с которой могу проводить незабываемое время. Я люблю играть в видео игры, хорошо знаю Питон и С++, интересуюсь веб-разработкой и дизайном.",
-        "Я душевный, интеллигентный, интровертный мужчина, охотный путешественник и большой любитель кино. Я чувствую себя комфортно со всеми и рад исследовать новое. У меня есть крепкие компьютерные навыки и уверенные знания в программировании, самоучитель C++, Python и веб-разработки.",
-        "Я социальный и энергичный мужчина, увлекающийся всеми аспектами жизни. Люблю путешествовать, любящий друга и защитник, социально активный и помогающий другим. Хорошо знаю Питон, С++, Rust и веб-распространение, а также люблю играть панк и хард-рок.")
+    user = User(
+            telegram_id=i,
+            telegram_handle=telegram_handle,
+            name=random.choice(names),
+            age=age,
+            sex=sex,
+            city=random.choice(CITIES),
+            relationship_goal=random.choice(RELATIONSHIP_GOALS),
+            photo=random.choice(photos),
+            self_descriptions=random.choice(descriptions),
+            min_preferred_age=0,
+            max_preferred_age=100,
+            )
 
-FEMALE_SELF_DESCRIPTIONS = (
-        "Привет! Я веселая и энергичная девушка, которая любит активный образ жизни. Обожаю путешествовать, открывать новые места и встречать интересных людей. Ищу партнера, с которым смогу делить свои приключения и создавать незабываемые воспоминания.",
+    
 
-"Приветствую! Я творческая и нежная девушка, увлекающаяся искусством и музыкой. Мои увлечения включают рисование, пение и танцы. Ищу вторую половинку, с кем смогу разделить свои творческие идеи и вместе создавать что-то прекрасное.",
 
-"Привет! Я интеллектуальная и целеустремленная девушка, увлеченная наукой и образованием. Люблю читать книги, учиться чему-то новому и развиваться как личность. Ищу партнера, с кем смогу вести умные и глубокие разговоры, а также поддерживать и вдохновлять друг друга на достижение целей.",
 
-"Привет! Я заботливая и семейно ориентированная девушка, ценящая домашний уют и теплые отношения. Обожаю готовить вкусные блюда, проводить время с близкими и заботиться о своих близких. Ищу партнера, с кем смогу создать крепкую и счастливую семью.",
 
-"Привет! Я спортивная и здоровая девушка, увлеченная фитнесом и заботой о своем теле. Люблю заниматься спортом, ходить в тренажерный зал и заниматься йогой. Ищу партнера, который разделяет мою страсть к здоровому образу жизни и готов вместе достигать новых спортивных целей.",)
 
-MIN_AGE = 18
-MAX_AGE = 30
+
+
+
+
 
 NAMES = MALE_NAMES + FEMALE_NAMES
 
