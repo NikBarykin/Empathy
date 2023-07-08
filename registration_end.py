cd171e118b0ad2dc8dd15581eba7f4f5a4feeabe
NikBarykin <barykin.nikita63@gmail.com>
2023-07-09 00:01:27 +0700

    registration end

---
+++ registration_end.py
@@ -0,0 +1,26 @@
+from user_state import UserState
+
+from matching.match import get_next_match
+
+from aiogram import Bot
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+from sqlalchemy.ext.asyncio import (
+        async_sessionmaker,
+        AsyncSession,
+    )
+
+
+async def process_end(
+        bot: Bot,
+        user_telegram_id: int,
+        state: FSMContext,
+        async_session: async_sessionmaker[AsyncSession],
+        ) -> None:
+    await get_next_match(
+            bot,
+            user_telegram_id,
+            async_session,
+            )
+    await state.set_state(UserState.registered)
