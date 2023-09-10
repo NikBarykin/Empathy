1da4012386f385fdd0a2f861b44fdac0ece864e9
root <root@44298.zetalink.ru>
2023-09-10 13:38:26 +0000

    updated hard-restart

--- hard_restart.sh
+++ hard_restart.sh
@@ -1,1 +1,1 @@
-redis-cli flushall && dropdb database.db && createdb database.db && python3.10 generate/__main__.py && python3.10 bot/__main__.py
+redis-cli flushall && dropdb database.db && createdb --template=template0 --locale=en_US.UTF-8 --encoding=UTF8 database.db && python3.10 generate/__main__.py && python3.10 bot/__main__.py
