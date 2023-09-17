8809ea4da1a59bbbda151dc80da192ada39ea4da
NikBarykin <barykin.nikita63@gmail.com>
2023-09-17 16:07:38 +0300

    take user's photo with the best quality

--- src/utils/message_value_getters/photo.py
+++ src/utils/message_value_getters/photo.py
@@ -4,4 +4,5 @@ from aiogram.types import Message
 
 async def first_photo_getter(message: Message):
     """Gets firs photo from presented in message"""
-    return message.photo[0].file_id
+    # -1, because we want the best quality of photo
+    return message.photo[-1].file_id
